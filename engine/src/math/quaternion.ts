22b9dcd649db876ad71f2d5b37bc869db4d0c38a
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-16 23:40:00 +0100

    Add orientationInDirection quat func from AI for Games book

--- engine/src/math/quaternion.ts
+++ engine/src/math/quaternion.ts
@@ -1,3 +1,10 @@
+import { Vector3 } from './vector';
+
 class Quaternion {
     constructor(public _x = 0, public _y = 0, public _z = 0, public _w = 0) {}
+
+    static orientationInDirection(axis: Vector3, angle: number) {
+        const sinAngle = Math.sin(angle / 2);
+        return new Quaternion(sinAngle * axis.x, sinAngle * axis.y, sinAngle * axis.z, Math.cos(angle / 2));
+    }
 }
