51977e8b8a0beb091f711d2c00be7c56c586197d
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-17 18:42:32 +0100

    Add conjugate to quaternion

--- engine/src/math/quaternion.ts
+++ engine/src/math/quaternion.ts
@@ -1,10 +1,14 @@
 import { Vector3 } from './vector';
 
 class Quaternion {
-    constructor(public _x = 0, public _y = 0, public _z = 0, public _w = 0) {}
+    constructor(public x = 0, public y = 0, public z = 0, public w = 0) {}
 
     static orientationInDirection(axis: Vector3, angle: number) {
         const sinAngle = Math.sin(angle / 2);
         return new Quaternion(sinAngle * axis.x, sinAngle * axis.y, sinAngle * axis.z, Math.cos(angle / 2));
+    }
+
+    conjugate() {
+        return new Quaternion(-this.x, -this.y, -this.z, this.w);
     }
 }
