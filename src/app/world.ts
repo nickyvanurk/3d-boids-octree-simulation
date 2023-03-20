5197ca2575bf1a8b37aec5e0f89be13a9bf092da
Nicky van Urk <nickyvanurk@gmx.com>
2023-03-20 23:10:06 +0100

    Reset resourec when back at home position

--- src/app/world.ts
+++ src/app/world.ts
@@ -47,6 +47,10 @@ export class World {
             this.currentAsteroid++;
         } else {
             this.ship.arrive(this.home, 45, 0);
+
+            if (this.ship.velocity.mag < 0.1 && this.ship.resource === 0) {
+                this.ship.resource = 0;
+            }
         }
 
         this.ship.update(dt);
